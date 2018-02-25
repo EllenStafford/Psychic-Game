72ef554db7d3ab3734e195dddf2e9fe592c639c7
EllenStafford <ehs@ehstafford.com>
2018-02-25 18:49:31 -0500

    2nd upload. game.js is working. index is not pretty but it works.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,12 @@
 //At the start of the game
 
+//Designate computer choices. √
+
+var computerOptions = [
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+    "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+    "u", "v", "w", "x", "y", "z"];
+
 //Wins = 0 √
 //Losses = 0 √
 //Number of User Guesses Left = 9 √
@@ -9,53 +16,47 @@ var userWin = 0;
 var userLoss = 0;
 var userGuessesLeft = 9;
 
-//Designate computer choices. √
-
-var computerOptions = [
-    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
-    "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
-    "u", "v", "w", "x", "y", "z"
-];
-
 //User makes a choice. √
 //Capture userGuess. √
 //Show userGuess.
 
-document.onkeyup = function(){
-    var userGuess = String.fromCharCode(event.keycode).toLowerCase();
+document.onkeyup = function(event){
+    var userGuess = event.key.toLowerCase();
     
-//Computer makes a random choice. √
-//Capture Computer's choice. √
+    //Computer makes a random choice. √
+    //Capture Computer's choice. √
 
     var computerGuess = computerOptions[Math.floor(Math.random()*computerOptions.length)];
 
-//Compare user's choice to the computer's choice. √
+    //Compare user's choice to the computer's choice. √
 
-//If the Result is a Win:
-//Add 1 to the userWin.
-//Reset userGuess to 9
-//Restart the game w/o refreshing the page.
+    //If the Result is a Win: √
+    //Add 1 to the userWin. √
+    //Reset userGuess to 9 √
+    //Restart the game w/o refreshing the page. √
 
-//If the Result is a Loss:
-//Subtract 1 from userGuess.
-//Add 1 to userLoss.
-//If userGuess = 0, then restart the game w/o a page refresh.
+    //If the Result is a Loss: √
+    //Subtract 1 from userGuess. √
+    //Add 1 to userLoss. √
+    //If userGuess = 0, then restart the game w/o a page refresh. √
 
-if (computerGuess === userGuess) {
-        userWin++ && (userGuessesLeft = 9);
-    } else userGuessesLeft - 1;
-
-    if (userGuessesLeft === 0){
-        userLoss++ && (userGuessesLeft = 9);
+ 
+    if (computerGuess === userGuess) { userWin++, (userGuessesLeft=9);
+    } else { userGuessesLeft--;
     }
 
-//Display the Result to the User.
+    if (userGuessesLeft === 0) { userLoss++, (userGuessesLeft=9);
+    }
+
+        
+
+//Display the Result to the User. √
 //Game start: "Guess what Letter I'm thinking of?"√
 
-var html =
+var html = "<h1>Guess What Letter I'm Thinking Of?<h1>" +
 "<p>You chose: " + userGuess + "</p>" +
 "<p>The computer chose: " + computerGuess + "</p>" +
-"<p>Guesses:  " + numberGuesses + "</p>" +
+"<p>Guesses:  " + userGuessesLeft + "</p>" +
 "<p>Wins:  " + userWin + "</p>" +
 "<p>Losses:  " + userLoss + "</p>";
 
@@ -63,10 +64,3 @@ var html =
 document.querySelector("#game").innerHTML = html;
 
 };
-
-
-
-
-//Loop the userChoice until userWin or userGuess = 0
-//Restart the game w/o page refresh.
-
